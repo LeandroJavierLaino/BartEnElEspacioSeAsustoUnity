5ef1221c94f877045bbc7ff08d50b873051bb1b0
LeandroJavierLaino <leanlaino@gmail.com>
2020-09-29 18:34:19 -0300

    - Strating modeling level. - Camera and player control. - adding some light to player and main room.

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public Transform transformPlayer;
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90f,90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transformPlayer.Rotate(mouseX * Vector3.up);
+
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public Rigidbody rigidbodyPlayer;
+    public float speed = 5f;
+    public float jumpForce = 32f;
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+
+        rigidbodyPlayer.velocity = move * speed  ;  
+        rigidbodyPlayer.AddForce(new Vector3(0f,Input.GetKeyDown(KeyCode.Space) && rigidbodyPlayer.velocity.y >= 0 ? jumpForce : 0f,0f),ForceMode.Impulse);     
+    }
+}
