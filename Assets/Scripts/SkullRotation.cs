ab857ef1168d254b335e86bb4d3e5b9d6867efd2
LeandroJavierLaino <leanlaino@gmail.com>
2020-11-11 23:50:59 -0300

    - Creating base for main menu. - Adding background and a transform animation enemy.

---
+++ Assets/Scripts/SkullRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullRotation : MonoBehaviour
+{
+    public float rotationSpeed = 15f;
+    public float updownvelocity = 3f;
+    public float amplitude = 1.25f;
+    public float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+        transform.Translate(Vector3.up * amplitude * Mathf.Sin(timer * updownvelocity));
+    }
+}
